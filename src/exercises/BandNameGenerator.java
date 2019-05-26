623ae582a8cbf6c70968ad6974910fa1582eb465
MilanZlaja1986 <milan.zlatkovic@hotmail.rs>
2019-05-26 19:49:40 +0200

    Implemented a random band name generator

--- src/exercises/BandNameGenerator.java
+++ src/exercises/BandNameGenerator.java
@@ -1,5 +1,13 @@
 package exercises;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.Random;
+
 /* 
  * Make 2 ArrayLists; 
  *      1 full of adjectives (e.g. slimy, blind)
@@ -8,5 +16,28 @@ package exercises;
  * When the program is run, one word is randomly selected from each list to generate a band name.
 */
 public class BandNameGenerator {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String>adjectives = new ArrayList<String>();
+		ArrayList<String>pluralNouns = new ArrayList<String>();
+		
+		adjectives.add("short");
+		adjectives.add("streched");
+		adjectives.add("curly");
+		
+		pluralNouns.add("shorts");
+		pluralNouns.add("pants");
+		pluralNouns.add("computers");
+		
+		int r1 = new Random().nextInt(3);
+		int r2 = new Random().nextInt(3);
+	//	System.out.println(r1);
+		
+		System.out.println("The " + adjectives.get(r1) + " " + pluralNouns.get(r2));
+		
+	}
+	
+
 
 }
