8076c5b962f9f5db0e5b84e260ec3b2c5db1ac2d
MilanZlaja1986 <49413344+MilanZlaja1986@users.noreply.github.com>
2019-06-01 13:45:55 +0200

    Add files via upload
    Changed code in SearchingAndSortingAlgorithms.

--- src/exercises/SearchingAndSortingAlgorithms.java
+++ src/exercises/SearchingAndSortingAlgorithms.java
@@ -3,6 +3,8 @@ package exercises;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class SearchingAndSortingAlgorithms extends TestCase  {
@@ -14,22 +16,22 @@ public class SearchingAndSortingAlgorithms extends TestCase  {
 
 	/* 1. Create a method that will find return the first index of a broken egg */
 
-	public void testSearch() {
-		List<String> eggs = Arrays.asList(new String[] { "whole", "whole", "whole", "whole", "cracked", "whole", "whole", "whole",
-				"cracked", "whole", "whole", "whole" });
+//	public void testSearch() {
+//		List<String> eggs = Arrays.asList(new String[] { "whole", "whole", "whole", "whole", "cracked", "whole", "whole", "whole",
+//				"cracked", "whole", "whole", "whole" });
 		//assertEquals(EXPECTED, ACTUAL)
-		assertEquals(4, Algorithms.findBrokenEgg(eggs));
-	}
-
-	/* 2. Count how many oysters contain pearls. */
-//	@Test
-//	public void testSearchAndAccumulate() throws Exception {
-//		List<Boolean> oysters = Arrays.asList(new Boolean[] { false, false, true, false, false, false, false, false, false, false, false,
-//				false, false, false, false, false, false, false, false, false, false, false, false, true });
-//		assertEquals(2, Algorithms.countPearls(oysters));
+//		assertEquals(4, Algorithms.findBrokenEgg(eggs));
 //	}
 
-	/* 3. Create a method that will return the height of the tallest person. */
+	/* 2. Count how many oysters contain pearls. */
+	//@Test
+	//public void testSearchAndAccumulate() throws Exception {
+	//	List<Boolean> oysters = Arrays.asList(new Boolean[] { false, false, true, false, false, false, false, false, true, false, false,
+	//			false, false, false, false, false, false, false, false, false, false, false, false, true });
+	//	assertEquals(3, Algorithms.countPearls(oysters));
+	//}
+
+	/* 3. Create a method that will return the height of the tallest person. */ 
 //	@Test
 //	public void testFindMaxiumum() throws Exception {
 //		List<Double> peeps = Arrays.asList(new Double[] { 5.7, 6.2, 3.4, 6.2, 5.0, 5.5, 4.7, 6.2, 3.4, 4.2, 5.0, 4.5, 5.1, 6.6 });
@@ -37,15 +39,15 @@ public class SearchingAndSortingAlgorithms extends TestCase  {
 //	}
 
 	/* 4. Create a method that will find the longest word. */
-//	@Test
-//	public void testFindMaxiumumString() throws Exception {
-//		List<String> words = Arrays.asList(new String[] { "accoutrements", "acumen", "anomalistic", "auspicious", "bellwether",
-//				"callipygian", "circumlocution", "concupiscent", "conviviality", "coruscant", "cuddlesome", "cupidity", "cynosure",
-//				"ebullient", "equanimity", "excogitate", "gasconading", "idiosyncratic", "luminescent", "magnanimous", "nidificate",
-//				"osculator", "parsimonious", "penultimate", "perfidiousness", "perspicacious", "proficuous", "remunerative", "saxicolous",
-//				"sesquipedalian", "superabundant", "unencumbered", "unparagoned", "usufruct", });
-//		assertEquals("circumlocution", Algorithms.findLongestWord(words));
-//	}
+	//@Test
+	//public void testFindMaxiumumString() throws Exception {
+	//	List<String> words = Arrays.asList(new String[] { "accoutrements", "acumen", "anomalistic", "auspicious", "bellwether",
+	//			"callipygian", "circumlocution", "concupiscent", "conviviality", "coruscant", "cuddlesome", "cupidity", "cynosure",
+	//			"ebullient", "equanimity", "excogitate", "gasconading", "idiosyncratic", "luminescent", "magnanimous", "nidificate",
+	//			"osculator", "parsimonious", "penultimate", "perfidiousness", "perspicacious", "proficuous", "remunerative", "saxicolous",
+	//			"sesquipedalian", "superabundant", "unencumbered", "unparagoned", "usufruct", });
+	//	assertEquals("circumlocution", Algorithms.findLongestWord(words));
+	//}
 
 	/*
 	 * 5. The Morse code for SOS is "... --- ..."
@@ -75,24 +77,25 @@ public class SearchingAndSortingAlgorithms extends TestCase  {
 	/* 8. Sort the DNA sequences by length from shortest to longest. */
 //	@Test
 //	public void testSortStringLength() throws Exception {
-//		List<String> unsortedSequences = Arrays.asList(new String[] { "ATAGCTGATCGTAGCTACGTACGATCG", "CATCGTACATGC", "TATGTGT",
-//				"GCTGATCGTGACTGTAC", "ACTGT" });
-//		List<String> sortedSequences = Arrays.asList(new String[] { "ACTGT", "TATGTGT", "CATCGTACATGC", "GCTGATCGTGACTGTAC",
-//				"ATAGCTGATCGTAGCTACGTACGATCG" });
+//		List<String> unsortedSequences = Arrays.asList(new String[] { "ATAGCTGATCGTAGCTACGTACGATCG", "CATCGTACATGC",
+//				"TATGTGT", "GCTGATCGTGACTGTAC", "ACTGT" });
+//		List<String> sortedSequences = Arrays.asList(new String[] { "ACTGT", "TATGTGT", "CATCGTACATGC",
+//				"GCTGATCGTGACTGTAC", "ATAGCTGATCGTAGCTACGTACGATCG" });
 //		assertEquals(sortedSequences, Algorithms.sortDNA(unsortedSequences));
 //	}
+		
 
 	/*
 	 * 9. Sort the words in alphabetical order. Your teacher may need to explain compareTo.
 	 */
-//	@Test
-//	public void testSortStringContents() throws Exception {
-//		List<String> words = Arrays.asList(new String[] { "aby", "dap", "alt", "alb", "ama", "ard", "ana", "ala", "awn", "dah", "bar",
-//				"bee", "bel", "bot", "bis", "cep", "alk", "cog", "col", "cwm", "dag", "ait", "dal", "daw" });
-//		assertEquals("aby", Algorithms.sortWords(words).get(0));
-//		assertEquals("bar", Algorithms.sortWords(words).get(10));
-//		assertEquals("daw", Algorithms.sortWords(words).get(words.size() - 1));
-//	}
+	@Test
+	public void testSortStringContents() throws Exception {
+		List<String> words = Arrays.asList(new String[] { "aby", "dap", "alt", "alb", "ama", "ard", "ana", "ala", "awn", "dah", "bar",
+				"bee", "bel", "bot", "bis", "cep", "alk", "cog", "col", "cwm", "dag", "ait", "dal", "daw" });
+		assertEquals("aby", Algorithms.sortWords(words).get(0));
+		assertEquals("bar", Algorithms.sortWords(words).get(10));
+		assertEquals("daw", Algorithms.sortWords(words).get(words.size() - 1));
+	}
 
 }
 
