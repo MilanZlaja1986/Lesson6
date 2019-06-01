7b5496860ec092d62339d76bb79da7553d7a9e2e
MilanZlaja1986 <49413344+MilanZlaja1986@users.noreply.github.com>
2019-06-01 13:46:58 +0200

    Add files via upload
    Changed code in StringReversal.

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -11,8 +11,28 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+		System.out.println(reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+	}
+	
+//	public String reverseMe(String string) {
+//		String reverseWord = "";
+//		char[] stringWord = string.toCharArray();
+//		for (int i = stringWord.length - 1; i >= 0; i--) {
+//			reverseWord += stringWord[i];
+//		}
+//		return reverseWord;
 //	}
-
+	
+	public String reverseMe(String string) {
+		String reverseWord = "";
+		char[] stringWord = string.toCharArray();
+		for (int i = stringWord.length - 1; i >= 0; i--) {
+			reverseWord += stringWord[i];
+		}
+		return reverseWord;
+	}
+	
+	
 }
