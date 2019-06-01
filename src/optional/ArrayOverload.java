334114ead121ae57fe408305e79877c6f97797ce
MilanZlaja1986 <49413344+MilanZlaja1986@users.noreply.github.com>
2019-06-01 13:52:46 +0200

    Add files via upload
    Changed code in ArrayOverload.

--- src/optional/ArrayOverload.java
+++ src/optional/ArrayOverload.java
@@ -1,7 +1,20 @@
 package optional;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /* Burst an ArrayList by putting so many things into it that you cause an OutOfMemory error. */
 
 public class ArrayOverload {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Double>listedDoubles = new ArrayList<Double>();
+		
+		while (true) {
+			listedDoubles.add(new Random().nextDouble());	
+		}
+		
+	}
 
 }
