b6436eba3342a5f0f9d034f45c7d5c2dcdb29d6d
MilanZlaja1986 <49413344+MilanZlaja1986@users.noreply.github.com>
2019-06-01 13:54:46 +0200

    Add files via upload
    Changed code in PeaceOnEarth.

--- src/optional/PeaceOnEarth.java
+++ src/optional/PeaceOnEarth.java
@@ -19,6 +19,14 @@ public class PeaceOnEarth {
 		truth.add('#');
 		truth.add('c');
 		truth.add('k');
+		
+		truth.remove(0);
+		truth.remove(2);
+		truth.remove(6);
+		truth.remove(7);
+		truth.remove(7);
+		
+		System.out.println(truth);
 
 		/* TODO: Remove the hash symbols and print out the truth. */
 		
