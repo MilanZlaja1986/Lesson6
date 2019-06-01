dd78ddf2989fd32877ad6ada5a1aea62df514362
MilanZlaja1986 <49413344+MilanZlaja1986@users.noreply.github.com>
2019-06-01 14:00:54 +0200

    Add files via upload
    Add a new class called Sorting.

---
+++ src/optional/Sorting.java
@@ -0,0 +1,18 @@
+package optional;
+
+public class Sorting {
+
+	public static String[] sort(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (array[j].compareTo(array[i]) > 0) {
+					String s = array[i];
+					array[i] = array[j];
+					array[j] = s;
+				}
+			}
+		}
+		return array;
+	}
+
+}
