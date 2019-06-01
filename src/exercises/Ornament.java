2bb38bbc15759c67aef178d1bdaf1c12e8bc41dc
MilanZlaja1986 <49413344+MilanZlaja1986@users.noreply.github.com>
2019-06-01 13:44:30 +0200

    Add files via upload
    Changed code in Ornament.

--- src/exercises/Ornament.java
+++ src/exercises/Ornament.java
@@ -1,15 +1,26 @@
 package exercises;
 
+import java.util.Random;
+
 /* 
  * Create a class called Decorator that contains an ArrayList called boxOfDecorations.
  *   1. In the Decorator class, hang all the ornaments in the box.
- *   2. Add a color to the Ornament class so that your program prints “Hanging a red ornament”, etc.
+ *   2. Add a color to the Ornament class so that your program prints â€œHanging a red ornamentâ€�, etc.
  */
 
 public class Ornament {
 	
+	String color;
+	
+	public Ornament() {
+		Random randomcolor = new Random();
+		String[] colors = {"Navajo white", "Amazon", "Artichoke", "Avocado", "Black", "Celtic blue"}; 
+		this.color = colors[randomcolor.nextInt(colors.length)];
+	}
+	
+	
 	public void hang(){
-		System.out.println("hanging an ornament");
+		System.out.println("Hanging an " + color + " ornament");
 	}
 
 }
